fc430db5ac4b6455c70b624f68eecc4e2249991a
RulinShao <srl0310@outlook.com>
2022-05-11 17:04:20 -0400

    minor change

--- fast_kdiga.py
+++ fast_kdiga.py
@@ -112,7 +112,6 @@ def main():
     # Training
     prev_robust_acc = 0.
     start_train_time = time.time()
-    logger.info('Epoch \t Seconds \t LR \t \t Train Loss \t Train Acc \t Test Loss \t Test Acc')
     for epoch in range(args.epochs):
         start_epoch_time = time.time()
         train_loss = 0
@@ -147,10 +146,10 @@ def main():
             grad_t_adv = torch.autograd.grad(loss_t_adv, delta)[0]
 
             # Align iff teacher predicts right
-            t_correct_ = t_correct[:, None].repeat(1, args.num_classes)
+            t_correct_ = t_correct[:X.size(0), None].repeat(1, args.num_classes)
             kd_loss = args.temp * args.temp * F.kl_div(F.log_softmax(output_s_adv / args.temp, dim=1) * t_correct_,
                                                        F.softmax(output_t_adv.detach() / args.temp, dim=1) * t_correct_)
-            t_correct_ = t_correct[:, None, None, None].repeat([1]+list(grad_t_adv.detach().shape[1:]))
+            t_correct_ = t_correct[:X.size(0), None, None, None].repeat([1]+list(grad_t_adv.detach().shape[1:]))
             grad_diff = torch.flatten((grad_s_adv - grad_t_adv) * t_correct_, start_dim=1)
             iga_loss = args.gama * torch.linalg.norm(grad_diff, ord=2, dim=1).mean()
             
@@ -177,6 +176,7 @@ def main():
         lr = scheduler.get_lr()[0]
         
         test_loss, test_acc = evaluate_standard(test_loader, model)
+        logger.info('Epoch \t Seconds \t LR \t \t Train Loss \t Train Acc \t Test Loss \t Test Acc')
         logger.info('Testing: %d \t %.1f \t \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f',
             epoch, epoch_time - start_epoch_time, lr, train_loss, train_acc, test_loss, test_acc)
     
